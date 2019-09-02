8c701f69b0c3cca81bc8d1defb69e80aa259bab6
DesDanielNacher <daniel.nacher@despegar.com>
2019-09-02 13:50:14 -0300

    test

--- src/main/java/com/bma/BmaServer.java
+++ src/main/java/com/bma/BmaServer.java
@@ -2,6 +2,8 @@ package com.bma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BmaServer {
@@ -9,7 +11,6 @@ public class BmaServer {
     public static void main(String[] args) {
         SpringApplication.run(BmaServer.class, args);
     }
-
     /*
     *
     * <a style="background-color:black;
@@ -37,4 +38,15 @@ public class BmaServer {
     *
     * */
 
+}
+
+@RestController
+class BmaController{
+
+    private final String VERSION = "1.0.0";
+    
+    @GetMapping("/")
+    String bma(){
+        return "bma v" + VERSION;
+    }
 }
